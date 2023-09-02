190e2386e20c6823e504dd091bd0709e83df2d2b
Daniel.Barton446 <daniel.barton446@gmail.com>
2023-09-02 12:19:31 -0500

    feat: enforces environments to implement display trait

--- src/environment/environment.rs
+++ src/environment/environment.rs
@@ -1,6 +1,8 @@
+use std::fmt::Display;
+
 use crate::{des::des::Scheduler, event::event::Event};
 
-pub trait Environment {
+pub trait Environment: Display {
     fn apply_event(&mut self, scheduler: &mut Scheduler, event: Box<dyn Event>);
     fn get_state(&self) -> String;
 }
