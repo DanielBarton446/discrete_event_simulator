23e8cea6b9014d5d780e60ceb0606578355d9a5e
Daniel.Barton446 <daniel.barton446@gmail.com>
2023-09-03 16:37:21 -0500

    feat: unload passengers event

---
+++ src/environment/bus_world/bus_world_events/unload_passengers.rs
@@ -0,0 +1,48 @@
+use serde::{Deserialize, Serialize};
+
+use crate::event::event::Event;
+
+pub struct UnloadPassengersEvent {
+    uid: usize,
+    timestamp: f64,
+    data: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct UnloadPassengersJson {
+    pub bus_uid: usize, // what else?
+}
+
+impl UnloadPassengersJson {
+    pub fn new(bus_uid: usize) -> Self {
+        Self { bus_uid }
+    }
+}
+
+impl UnloadPassengersEvent {
+    pub fn new(uid: usize, timestamp: f64, data: String) -> UnloadPassengersEvent {
+        UnloadPassengersEvent {
+            uid,
+            timestamp,
+            data,
+        }
+    }
+}
+
+impl Event for UnloadPassengersEvent {
+    fn get_event_type(&self) -> &str {
+        "UnloadPassengers"
+    }
+
+    fn get_uid(&self) -> usize {
+        self.uid
+    }
+
+    fn get_time_stamp(&self) -> f64 {
+        self.timestamp
+    }
+
+    fn get_data(&self) -> Result<String, serde_json::Error> {
+        Ok(self.data.clone())
+    }
+}
