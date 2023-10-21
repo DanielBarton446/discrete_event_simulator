605805d85e0efd519961da8ceddc0a272f5a8b78
Daniel Barton <daniel.barton446@gmail.com>
2023-10-21 10:17:51 -0500

    doc: scheduler documentation

--- src/des/scheduler.rs
+++ src/des/scheduler.rs
@@ -1,11 +1,13 @@
-// Scheduler
-//
-//
-//
+//! Responsible for managing time-sorted queue of events
+//! and providing the next event to be processed.
 
 use crate::event::schedulable::SchedulableEvent;
 use std::collections::BinaryHeap;
 
+/// Binary Heap implementation based queue for events.
+/// Maintains current time and runtime.
+/// This queue is able to support any event which
+/// implements the [SchedulableEvent] trait.
 pub struct Scheduler {
     pub current_time: usize,
     pub runtime: usize,
@@ -13,6 +15,13 @@ pub struct Scheduler {
 }
 
 impl Scheduler {
+    /// Create a new scheduler with a given runtime.
+    ///
+    /// ```
+    /// use discrete_event_simulator::des::scheduler::Scheduler;
+    /// // Create a scheduler with a runtime of 100
+    /// let scheduler = Scheduler::new(100);
+    /// ```
     pub fn new(runtime: usize) -> Scheduler {
         Scheduler {
             current_time: 0,
@@ -21,14 +30,30 @@ impl Scheduler {
         }
     }
 
+    /// Add an event to the scheduler.
+    /// ```
+    /// use discrete_event_simulator::des::scheduler::Scheduler;
+    /// use discrete_event_simulator::event::schedulable::SchedulableEvent;
+    /// use discrete_event_simulator::environment::bus_world::bus_world_events::terminal_event::*;
+    /// let mut scheduler = Scheduler::new(100);
+    /// // create a terminal even for the bus world environment and schedule it
+    /// let event = TerminalEvent::new(0, 0, String::from("Terminal Event Data"));
+    /// scheduler.add_event(Box::new(event));
+    /// ```
     pub fn add_event(&mut self, event: Box<dyn SchedulableEvent>) {
-        // somwhere we need to add logic for the delay of running the event vs
-        // the absolute current time in the simulation that the event takes place.
         self.event_queue.push(event);
     }
 
+    /// Get the next event from the scheduler.
+    /// ```
+    /// use discrete_event_simulator::des::scheduler::Scheduler;
+    /// let mut scheduler = Scheduler::new(100);
+    /// // get the next event from the scheduler (there is none)
+    /// let event = scheduler.next_event();
+    /// assert!(event.is_none());
+    /// ```
     pub fn next_event(&mut self) -> Option<Box<dyn SchedulableEvent>> {
-        let event = match self.event_queue.pop() {
+        match self.event_queue.pop() {
             Some(event) => {
                 if event.get_time_stamp() > self.runtime {
                     return None;
@@ -37,7 +62,6 @@ impl Scheduler {
                 Some(event)
             }
             None => None,
-        };
-        return event;
+        }
     }
 }
